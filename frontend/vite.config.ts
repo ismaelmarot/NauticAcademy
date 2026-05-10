04b4869385d87081201beeaef474a85e4ae39d4d
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-05-10 10:18:31 -0300

    fix: make app-icon square, add proper PWA sizes for iOS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -17,12 +17,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/images/app_icons/app-icon.png',
+            src: '/images/app_icons/app-icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/images/app_icons/app-icon.png',
+            src: '/images/app_icons/app-icon-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
